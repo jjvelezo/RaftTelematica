67df41f6869d045a073c268f0748848024843fa3
unknown <juanjosev2003@hotmail.es>
2024-10-10 20:39:39 -0500

    :bug: Corrercion liders, conexiones, etc

--- db_server.py
+++ db_server.py
@@ -18,7 +18,9 @@ LEADER_ID = None
 TIMEOUT = random.uniform(0.15, 0.3)
 
 
-OTHER_DB_NODES = ['10.0.2.250', '10.0.2.234']
+OTHER_DB_NODES = ['10.0.2.162', '10.0.2.234']
+#OTHER_DB_NODES = ['10.0.2.250', '10.0.2.234']
+#OTHER_DB_NODES = ['10.0.2.250', '10.0.2.162']
 
 class DatabaseService(service_pb2_grpc.DatabaseServiceServicer):
 
@@ -76,11 +78,17 @@ class DatabaseService(service_pb2_grpc.DatabaseServiceServicer):
         return service_pb2.VoteResponse(granted=False)
 
     def AppendEntries(self, request, context):
-        global ROLE, LEADER_ID, TIMEOUT
-        LEADER_ID = request.leader_id
-        TIMEOUT = random.uniform(0.15, 0.3) 
-        print(f"[{ROLE}] - Received heartbeat from leader {LEADER_ID}")
+        global ROLE, LEADER_ID, CURRENT_TERM, TIMEOUT
+        if request.term >= CURRENT_TERM:
+            CURRENT_TERM = request.term
+            LEADER_ID = request.leader_id
+            ROLE = 'follower'
+            TIMEOUT = random.uniform(0.15, 0.3)  # Restablecer el timeout
+            print(f"[{ROLE}] - Received heartbeat from leader {LEADER_ID}")
+        else:
+            print(f"[{ROLE}] - Ignoring heartbeat from leader {request.leader_id} due to lower term")
         return service_pb2.AppendEntriesResponse(success=True)
+
 
 def start_election():
     global ROLE, CURRENT_TERM, VOTED_FOR, LEADER_ID
@@ -92,11 +100,11 @@ def start_election():
             print(f"[{ROLE}] - Timeout expired, starting election")
             ROLE = 'candidate'
             CURRENT_TERM += 1
-            VOTED_FOR = None
+            VOTED_FOR = 'self'
             LEADER_ID = None
 
-            # Pedir votos a los otros nodos y se vota el mismo
-            vote_count = 1 
+            # Pedir votos a los otros nodos
+            vote_count = 1  # Se vota a sí mismo
             for node_ip in OTHER_DB_NODES:
                 try:
                     channel = grpc.insecure_channel(f'{node_ip}:50051')
@@ -108,7 +116,7 @@ def start_election():
                 except Exception as e:
                     print(f"[{ROLE}] - Error contacting node {node_ip}: {e}")
 
-            # Si consigue la mayoria de votos se convierte en lider
+            # Si consigue la mayoría de votos se convierte en líder
             if vote_count > (len(OTHER_DB_NODES) + 1) // 2:
                 print(f"[{ROLE}] - Became leader for term {CURRENT_TERM}")
                 ROLE = 'leader'
@@ -116,6 +124,8 @@ def start_election():
                 start_heartbeats()
             else:
                 ROLE = 'follower'
+                LEADER_ID = None
+
 
 def start_heartbeats():
     global LEADER_ID, ROLE, TIMEOUT
