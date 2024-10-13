209d152dc5f8ed983c8c6253af4e399fbd2e229b
unknown <juanjosev2003@hotmail.es>
2024-10-13 16:19:51 -0500

    Vuelta atras

--- db_server.py
+++ db_server.py
@@ -70,12 +70,12 @@ class DatabaseService(service_pb2_grpc.DatabaseServiceServicer):
                         print(f"[{ROLE}] - Write operation failed: ID already exists")
                         return service_pb2.WriteResponse(status="ERROR: ID ya existente")
 
-
+            # Escribe en el archivo CSV del líder
             with open(DB_FILE, mode='a') as csv_file:
                 writer = csv.writer(csv_file)
                 writer.writerow(data)
             
-            print(f"[{ROLE}] - Write operation completed")
+            print(f"[{ROLE}] - Write operation completed. Data: {data}")
 
             # Replicar los datos a los seguidores
             self.replicate_to_followers(data)
@@ -84,6 +84,7 @@ class DatabaseService(service_pb2_grpc.DatabaseServiceServicer):
         else:
             print(f"[{ROLE}] - Write operation attempted on follower - Redirect to leader required")
             return service_pb2.WriteResponse(status="ERROR: Cannot write to follower")
+
 
     def ReplicateData(self, request, context):
         global ROLE
@@ -96,6 +97,7 @@ class DatabaseService(service_pb2_grpc.DatabaseServiceServicer):
                     reader = csv.reader(csv_file)
                     rows = [','.join(row) for row in reader]
                     full_data = "\n".join(rows)
+                    print(f"[{ROLE}] - Data to replicate: {full_data}")
                 
                 # Escribir los datos en el nodo de destino
                 with open(DB_FILE, mode='w', newline='') as csv_file:
@@ -113,13 +115,18 @@ class DatabaseService(service_pb2_grpc.DatabaseServiceServicer):
             return service_pb2.WriteResponse(status="ERROR: No replication data provided")
 
 
+
     def replicate_to_followers(self, data):
+        global ROLE
+        print(f"[{ROLE}] - Initiating replication to followers")
+
         for follower_ip in OTHER_DB_NODES:
             try:
                 channel = grpc.insecure_channel(f'{follower_ip}:50051')
                 stub = service_pb2_grpc.DatabaseServiceStub(channel)
                 replicate_request = service_pb2.WriteRequest(data=','.join(data))
                 response = stub.ReplicateData(replicate_request)
+
                 if response.status == "SUCCESS":
                     print(f"[{ROLE}] - Data successfully replicated to {follower_ip}")
                 else:
